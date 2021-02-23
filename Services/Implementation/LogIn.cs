e4c3187c1f3d3e696b78971b4bb5815139c77f1e
nafis-sadik <nafis_sadik@outlook.com>
2021-02-23 18:23:15 +0600

    Working on log in service

---
+++ Services/Implementation/LogIn.cs
@@ -0,0 +1,15 @@
+﻿using Services.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Implementation
+{
+    class LogIn : ILogIn
+    {
+        public bool UserLogIn(string UserName, string Password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
