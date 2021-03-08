6a1f367f551f18d13ae0a275700c2d792ffbe56c
Nafis Sadik <nafis_sadik@outlook.com>
2021-03-08 23:06:15 +0600

    DB Connected Succeccfully

---
+++ Entities/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Entities
+{
+    public partial class Product
+    {
+        public Product()
+        {
+            ShopProducts = new HashSet<ShopProduct>();
+        }
+
+        public decimal ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductAttribute { get; set; }
+
+        public virtual ICollection<ShopProduct> ShopProducts { get; set; }
+    }
+}
---
+++ Entities/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Entities
+{
+    public partial class User
+    {
+        public User()
+        {
+            Orders = new HashSet<Order>();
+        }
+
+        public decimal UserId { get; set; }
+        public string UserName { get; set; }
+        public string PhoneNumber { get; set; }
+        public decimal? UserTypeId { get; set; }
+        public string ProfilePicLoc { get; set; }
+
+        public virtual CommonCode UserType { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
